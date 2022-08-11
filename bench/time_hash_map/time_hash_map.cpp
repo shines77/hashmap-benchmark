4de294e0469dd6cf0aabc317c8a7713c29b42015
GuoXH <gz_shines@msn.com>
2022-08-11 14:14:11 +0800

    time_hash_map.cpp: 暂时关闭 EMHash7 的测试
    Signed-off-by: GuoXH <gz_shines@msn.com>

--- bench/time_hash_map/time_hash_map.cpp
+++ bench/time_hash_map/time_hash_map.cpp
@@ -91,7 +91,7 @@
 #define USE_SKA_FLAT_HASH_MAP       1
 #define USE_SKA_BYTELL_HASH_MAP     0
 #define USE_EMHASH5_HASH_MAP        1
-#define USE_EMHASH7_HASH_MAP        1
+#define USE_EMHASH7_HASH_MAP        0
 #define USE_ABSL_FLAT_HASH_MAP      1
 #define USE_ABSL_NODE_HASH_MAP      0
 
