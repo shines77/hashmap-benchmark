742677546f09f4070531b2fad0d7a670ebe0a58f
shines77 <gz_shines@msn.com>
2025-01-23 05:54:47 +0800

    尝试修正 absl 的编译错误
    Signed-off-by: shines77 <gz_shines@msn.com>

--- bench/jackson_bench/hashmaps/absl_flat_hash_map/hashmap_wrapper.h
+++ bench/jackson_bench/hashmaps/absl_flat_hash_map/hashmap_wrapper.h
@@ -2,16 +2,16 @@
 // Copyright (c) 2024 Jackson L. Allan.
 // Distributed under the MIT License (see the accompanying LICENSE file).
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wpedantic"
-#pragma GCC diagnostic ignored "-Woverflow"
+//#pragma GCC diagnostic push
+//#pragma GCC diagnostic ignored "-Wpedantic"
+//#pragma GCC diagnostic ignored "-Woverflow"
 //#include <absl/container/internal/raw_hash_set.cc>
 //#include <absl/base/internal/raw_logging.cc>
 //#include <absl/hash/internal/hash.cc>
 //#include <absl/hash/internal/city.cc>
 //#include <absl/hash/internal/low_level_hash.cc>
 #include <absl/container/flat_hash_map.h>
-#pragma GCC diagnostic pop
+//#pragma GCC diagnostic pop
 
 template <typename BluePrint>
 struct absl_flat_hash_map
@@ -21,6 +21,7 @@ struct absl_flat_hash_map
 
     struct hash {
         using is_avalanching = void;
+        using argument_type = key_type;
         using result_type = std::size_t;
 
         std::size_t operator () (const key_type & key) const {
--- bench/jackson_bench/hashmaps/ankerl_unordered_dense/hashmap_wrapper.h
+++ bench/jackson_bench/hashmaps/ankerl_unordered_dense/hashmap_wrapper.h
@@ -12,6 +12,7 @@ struct ankerl_unordered_dense
 
     struct hash {
         using is_avalanching = void;
+        using argument_type = key_type;
         using result_type = std::size_t;
 
         std::size_t operator () (const key_type & key) const
